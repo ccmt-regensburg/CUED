d71b17494fb42017def75ea7f66f38a844e66d9b
Invarianz <invarianztheorem@web.de>
2019-11-26 20:29:09 +0000

    Taking full wave-function derivatives performance is fine. Removed special norm treatment.

--- hfsbe/examples/systems.py
+++ hfsbe/examples/systems.py
@@ -18,30 +18,47 @@ class TwoBandSystems():
         self.sy = Matrix([[0, -I], [I, 0]])
         self.sz = Matrix([[1, 0], [0, -1]])
 
-        self.kx = Symbol('kx')
-        self.ky = Symbol('ky')
+        self.kx = Symbol("kx")
+        self.ky = Symbol("ky")
     
-    def __construct_energies(self, ho, hx, hy, hz):
+    def __eigensystem(self, ho, hx, hy, hz):
         """
         Generic form of energies in a two band Hamiltonian
+
+        Parameters
+        ----------
+        ho, hx, hy, hz : Symbol
+            Hamiltonian part proportional to unit, sx, sy, sz Pauli matrix
+
+        Returns
+        -------
+        e : list of Symbol
+            Valence and conduction band energies; in this order
+        wf : list of Symbol
+            Valence and conduction band wave function; in this order
         """
-        return [ho - sqrt(hx**2 + hy**2 + hz**2),
-                ho + sqrt(hx**2 + hy**2 + hz**2)]
         
-    def __construct_wavefunction(self, hx, hy, hz):
-        """
-        Generic form of wave functions in a two band Hamiltonian
-        """
-        return 
-            
+        esoc = sqrt(hx**2 + hy**2 + hz**2)
+        e = [ho - esoc, ho + esoc]
+                
+        wfv = Matrix([-hx + I*hy, hz + esoc])
+        wfc = Matrix([hz + esoc, hx + I*hy])
+        wfv_h = Matrix([-hx - I*hy, hz + esoc])
+        wfc_h = Matrix([hz + esoc, hx - I*hy])
+        norm = sqrt(2*(esoc + hz)*esoc)
+        
+        wf = [wfv/norm, wfv_h/norm, wfc/norm, wfc_h/norm]
+        
+        return e, wf
+                           
     def haldane(self):
         """
         Haldane model
         """
-        t1 = Symbol('t1')
-        t2 = Symbol('t2')
+        t1 = Symbol("t1")
+        t2 = Symbol("t2")
         m = Symbol('m')
-        phi = Symbol('phi')
+        phi = Symbol("phi")
 
         a1 = self.kx
         a2 = -1/2 * self.kx + sqrt(3)/2 * self.ky
@@ -56,8 +73,8 @@ class TwoBandSystems():
         hy = t1*sin(a1)+sin(a2)+sin(a3)
         hz = m - 2*t2*sin(phi)*(sin(b1)+sin(b2)+sin(b3))
 
-        e = self.__construct_energies(ho, hx, hy, hz)
-        wf = self.__construct_wavefunction(hx, hy, hz)
+        e, wf = self.__eigensystem(ho, hx, hy, hz)
+
         h = ho*self.so + hx*self.sx + hy*self.sy + hz*self.sz
 
         return h, e, wf
