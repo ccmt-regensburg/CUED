983b527f7821258b9c8aa33af9a12683ca9e4ee7
Invarianz <invarianztheorem@web.de>
2019-12-01 15:00:36 +0000

    Started wrapping function for Brillouin zone.

--- hfsbe/dipole/symbolic_dipole.py
+++ hfsbe/dipole/symbolic_dipole.py
@@ -1,7 +1,7 @@
 import sympy as sp
+import numpy as np
 
 import hfsbe.check.symbolic_checks as sck
-import hfsbe.dipole.interface as itf
 
 
 class SymbolicDipole():
@@ -44,10 +44,65 @@ class SymbolicDipole():
         dUy = sp.diff(self.U, self.ky)
         return self.U_h * dUx, self.U_h * dUy
 
-    @property
-    def Ax_numeric(self):
-        itf.to_numpy_function(self.Ax)
+    def symbolic_to_numeric(self, b1=None, b2=None, interpolation_ratio=1.0):
+        """
+        Transforms the symbolic expression for the
+        berry connection/dipole moment matrix to an expression
+        that can be evaluated by numpy.
+        If the reciprocal lattice vectors are given it creates a
+        Brillouin zone around the symbolic Hamiltonian. Values outside
+        of that zone are returned as np.nan.
+        The interpolation ratio (ipr) determines the part of the Brillouin
+        zone the symbolic Hamiltonian can be defined on. Outside of
+        this region up to the Brillouin zone boundaries the
+        dipole moments will be interpolated by constant values
+        given at the edge of the small zone given by ipr*b1 + ipr*b2
 
-    @property
-    def Ay_numeric(self):
-        itf.to_numpy_function(self.Ay) 
+        Parameters:
+        b1, b2 : np.ndarray
+            reciprocal lattice vector
+        interpolation_ratio : float
+            percentile portion of reciprocal lattice vectors
+        """
+        ipr = interpolation_ratio
+
+        if (b1 is None or b2 is None):
+            return to_numpy_function(self.Ax), \
+                to_numpy_function(self.Ay)
+        else:
+            return self.__add_brillouin_zone(b1, b2, ipr)
+
+    def __add_brillouin_zone(self, b1, b2, ip):
+        """
+        Add the brillouin zone to the symbolic dipole moments and
+        return numpy callable function.
+        """
+        Ax_n = to_numpy_function(self.Ax)
+        Ay_n = to_numpy_function(self.Ay)
+        
+        def brillouin_zone_function(**kwargs):
+            kmat = np.vstack(kwargs.pop('kx'), kwargs.pop('ky'))
+            b1.dot(kmat)
+            b2.dot(kmat)
+
+            
+            Ax_n(kwargs)
+            Ay_n(kwargs)
+
+
+def to_numpy_function(sf):
+    """
+    Converts a simple sympy function/matrix to a function/matrix
+    callable by numpy
+    """
+
+    return sp.lambdify(sf.free_symbols, sf, "numpy")
+
+
+def list_to_numpy_functions(sf):
+    """
+    Converts a list of sympy functions/matrices to a list of numpy
+    callable functions/matrices
+    """
+
+    return [to_numpy_function(sfn) for sfn in sf]
