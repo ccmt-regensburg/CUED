646eff3e1e3c74c78d6052c30036daab783963e5
Invarianz <invarianztheorem@web.de>
2019-12-03 20:15:45 +0000

    Found bug in the coordinate conversion. Now everything works as expected.

--- hfsbe/lattice/lattice.py
+++ hfsbe/lattice/lattice.py
@@ -3,13 +3,15 @@ import numpy as np
 
 def to_reciprocal_coordinates(kx, ky, b1, b2):
     kmat = np.vstack((kx, ky))
-    a1 = b1.dot(kmat)/(np.linalg.norm(b1)**2)
-    a2 = b2.dot(kmat)/(np.linalg.norm(b2)**2)
-    return a1, a2
+    bmat = np.array([[b1[0], b2[0]],
+                     [b1[1], b2[1]]])
+    amat = np.linalg.inv(bmat).dot(kmat)
+    return amat[0], amat[1]
 
 
 def to_cartesian_coordinates(a1, a2, b1, b2):
-    kmat = a1*b1[:, np.newaxis] + a2*b2[:, np.newaxis]
-    kx = kmat[0]
-    ky = kmat[1]
-    return kx, ky
+    amat = np.vstack((a1, a2))
+    bmat = np.array([[b1[0], b2[0]],
+                     [b1[1], b2[1]]])
+    kmat = bmat.dot(amat)
+    return kmat[0], kmat[1]
