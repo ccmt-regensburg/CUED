548618c44b014f46956b7d595a339754b52bcf74
Invarianz <invarianztheorem@web.de>
2020-01-14 15:14:22 +0000

    Trial for the derivatives.

--- scripts/bite_symbolic.py
+++ scripts/bite_symbolic.py
@@ -1,14 +1,17 @@
 import numpy as np
+import sympy as sp
+
+import matplotlib.pyplot as plt
 
 from hfsbe.example import BiTe
-from hfsbe.dipole import SymbolicDipole
-
+# from hfsbe.dipole import SymbolicDipole
 
 b1 = 2*np.pi*np.array([1/np.sqrt(3), 1])
 b2 = 2*np.pi*np.array([1/np.sqrt(3), -1])
 
 # kinit = np.linspace(-2*np.pi, 2*np.pi, 50)
-kinit = np.linspace(-0.04, 0.04, 501)
+N = 500
+kinit = np.linspace(-0.10, 0.10, N)
 
 # Full kgrid
 kmat = np.array(np.meshgrid(kinit, kinit)).T.reshape(-1, 2)
@@ -25,15 +28,28 @@ C0 = 0
 C2 = 0
 vf = 1
 
-bite = BiTe(A=A, R=R, C0=C0, C2=C2, vf=vf)
-h, ef, wf, ediff = bite.eigensystem(gidx=1)
+bite_one = BiTe(A=0, R=1, C0=0, C2=0, vf=1)
+h_one, ef_one, wf_one, ediff_one = bite_one.eigensystem(gidx=1)
+
+bite_two = BiTe(A=1, R=1, C0=0, C2=0, vf=0)
+h_two, ef_two, wf_two, ediff_two = bite_two.eigensystem(gidx=1)
+
+sp.pprint(ediff_one[0])
+sp.pprint(ediff_two[0])
+
+
+ediff_one_eval = bite_one.evaluate_ederivative(kinit, np.zeros(N))
+ediff_two_eval = bite_two.evaluate_ederivative(kinit, np.zeros(N))
+plt.plot(kinit, ediff_one_eval[0])
+plt.plot(kinit, ediff_two_eval[0])
+plt.show()
 # bite.evaluate_energy(kx, ky)
 # bite.evaluate_ederivative(kx, ky)
 # bite.plot_energies_3d(kx, ky)
 # bite.plot_energies_contour(kx, ky)
-dip = SymbolicDipole(h, ef, wf)
-Ax, Ay = dip.evaluate(kx, ky)
-print(np.shape(Ax))
+# dip = SymbolicDipole(h, ef, wf)
+# Ax, Ay = dip.evaluate(kx, ky)
+# print(np.shape(Ax))
 # dip.plot_dipoles(kx, ky)
 
 # plt.plot_dipoles(kx, ky, Ax, Ay, 'BiTe')
