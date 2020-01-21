e458e4f6aff47b4ac26ebafadf7d0d6663c31372
Invarianz <invarianztheorem@web.de>
2020-01-21 13:25:30 +0000

    Added simple loop

--- scripts/bite_symbolic.py
+++ scripts/bite_symbolic.py
@@ -12,44 +12,46 @@ def kmat(kinit):
     return kx, ky
 
 
-def topological(kx, ky, energy=False, ediff=False, dipole=False):
-    kx, ky = kmat(kinit)
+def topological(kx, ky, eflag=False, edflag=False, dipflag=False):
     bite = BiTe(A=0.1974, R=11.06, C0=0, C2=0)
     h, ef, wf, ediff = bite.eigensystem()
 
-    if (energy):
+    if (eflag):
         bite.evaluate_energy(kx, ky)
         bite.plot_bands_3d(kx, ky)
         bite.plot_bands_contour(kx, ky)
 
-    if (ediff):
+    if (edflag):
+        print(ediff)
+        print("Hello World")
         bite.evaluate_ederivative(kx, ky)
         bite.plot_bands_derivative(kx, ky)
 
-    if (dipole):
+    if (dipflag):
         dip = SymbolicDipole(h, ef, wf)
-        Ax, Ay = dip.evaluate(kx, ky)
-        dip.plot_dipoles(kx, ky)
         curv = SymbolicCurvature(dip.Ax, dip.Ay)
-        curv.evaluate(kx, ky)
-        curv.plot_curvature_contour(kx, ky)
+        for i in range(2000):
+            print("Round: ", i)
+            kx = i*0.0001*np.ones(100)
+            ky = np.zeros(100)
+            b = curv.evaluate(kx, ky)
+            b[0]
 
 
-def trivial(kx, ky, energy=False, ediff=False, dipole=False):
-    kx, ky = kmat(kinit)
+def trivial(kx, ky, eflag=False, edflag=False, dipflag=False):
     bite = BiTeTrivial(R=11.06, C0=0, C2=0, vf=0.1974)
     h, ef, wf, ediff = bite.eigensystem(gidx=1)
 
-    if (energy):
+    if (eflag):
         bite.evaluate_energy(kx, ky)
         bite.plot_bands_3d(kx, ky)
         bite.plot_bands_contour(kx, ky)
 
-    if (ediff):
+    if (edflag):
         bite.evaluate_ederivative(kx, ky)
         bite.plot_bands_derivative(kx, ky)
 
-    if (dipole):
+    if (dipflag):
         dip = SymbolicDipole(h, ef, wf)
         Ax, Ay = dip.evaluate(kx, ky)
         dip.plot_dipoles(kx, ky)
@@ -58,8 +60,8 @@ def trivial(kx, ky, energy=False, ediff=False, dipole=False):
 
 
 if __name__ == "__main__":
-    N = 200
+    N = 21
     kinit = np.linspace(-0.10, 0.10, N)
     kx, ky = kmat(kinit)
-    topological(kx, ky, energy=True)
+    topological(kx, ky, dipflag=True)
     # trivial(kx, ky, energy=True, ediff=True, dipole=True)
