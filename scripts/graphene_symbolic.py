58f75ddcb3bedb645699fa37f8f717c439660800
Patrick Grössing <invarianztheorem@web.de>
2020-02-16 20:02:10 +0000

    Renewed Graphene script.

--- scripts/graphene_symbolic.py
+++ scripts/graphene_symbolic.py
@@ -1,27 +1,39 @@
 import numpy as np
 
-from hfsbe.brillouin import in_brillouin
 from hfsbe.example import Graphene
-import hfsbe.dipole as dip
+from hfsbe.dipole import SymbolicDipole
 
-import plot_dipoles as plt
 
 b1 = 2*np.pi*np.array([1/np.sqrt(3), 1])
 b2 = 2*np.pi*np.array([1/np.sqrt(3), -1])
 
-kinit = np.linspace(-2*np.pi, 2*np.pi, 32)
 
-# Full kgrid
-kmat = np.array(np.meshgrid(kinit, kinit)).T.reshape(-1, 2)
-kx = kmat[:, 0]
-ky = kmat[:, 1]
+def kmat(kinit):
+    kmat = np.array(np.meshgrid(kinit, kinit)).T.reshape(-1, 2)
+    kx = kmat[:, 0]
+    ky = kmat[:, 1]
+    return kx, ky
 
-inbz = in_brillouin(kx, ky, b1, b2)
-kx = kx[inbz]
-ky = ky[inbz]
 
-h, ef, wf, ed = Graphene().eigensystem()
-dipole = dip.SymbolicDipole(h, ef, wf)
+def topological(kx, ky, eflag=False, edflag=False, dipflag=False):
+    graphene = Graphene(t=1)
+    h, ef, wf, ediff = graphene.eigensystem(gidx=1)
 
-Ax, Ay = dipole.evaluate(kx, ky, t=1, b1=b1, b2=b2)
-plt.plot_dipoles(kx, ky, Ax, Ay, 'Graphene')
+    if (eflag):
+        graphene.evaluate_energy(kx, ky)
+        graphene.plot_bands_3d(kx, ky)
+        graphene.plot_bands_contour(kx, ky)
+    if (edflag):
+        graphene.evaluate_ederivative(kx, ky)
+        graphene.plot_bands_derivative(kx, ky)
+    if (dipflag):
+        dip = SymbolicDipole(h, ef, wf)
+        Ax, Ay = dip.evaluate(kx, ky)
+        dip.plot_dipoles(kx, ky)
+
+
+if __name__ == "__main__":
+    N = 40
+    kinit = np.linspace(-2*np.pi, 2*np.pi, N)
+    kx, ky = kmat(kinit)
+    topological(kx, ky, eflag=True, dipflag=True)
