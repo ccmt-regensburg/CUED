Removed directionality bug in the dipole moments. Now they fullfill general analytical formula.

scripts/bite_symbolic.py
@@ -14,7 +14,7 @@ def kmat(kinit):
 
 def topological(kx, ky, eflag=False, edflag=False, dipflag=False):
     # bite = BiTe(A=0.1974, R=11.06, C0=0, C2=0)
-    bite = BiTe(A=10, R=0, C0=0, C2=0)
+    bite = BiTe(A=0.1974, R=11.06, C0=0, C2=0)
     h, ef, wf, ediff = bite.eigensystem(gidx=0)
 
     if (eflag):
@@ -52,8 +52,9 @@ def trivial(kx, ky, eflag=False, edflag=False, dipflag=False):
 
 
 if __name__ == "__main__":
-    N = 20
-    kinit = np.linspace(-0.02, 0.02, N)
+    N = 40
+    # kinit = np.linspace(-0.02, 0.02, N)
+    kinit = np.linspace(-0.1, 0.1, N)
     kx, ky = kmat(kinit)
-    topological(kx, ky, eflag=True, dipflag=True)
+    topological(kx, ky, eflag=False, edflag=False, dipflag=True)
     # trivial(kx, ky, energy=True, ediff=True, dipole=True)

scripts/dirac_symbolic.py
@@ -5,7 +5,6 @@ from hfsbe.dipole import SymbolicDipole
 
 
 def kmat(kinit):
-    kinit = np.linspace(-2*np.pi, 2*np.pi, 20)
     kmat = np.array(np.meshgrid(kinit, kinit)).T.reshape(-1, 2)
     kx = kmat[:, 0]
     ky = kmat[:, 1]
@@ -13,7 +12,7 @@ def kmat(kinit):
 
 
 def topological(kx, ky, eflag=False, edflag=False, dipflag=False):
-    dirac = Dirac(m=0)
+    dirac = Dirac(m=1)
     h, ef, wf, ediff = dirac.eigensystem(gidx=1)
 
     if (eflag):
@@ -31,6 +30,6 @@ def topological(kx, ky, eflag=False, edflag=False, dipflag=False):
 
 if __name__ == "__main__":
     N = 20
-    kinit = np.linspace(-0.2, 0.2, N)
+    kinit = np.linspace(-1.0, 1.0, N)
     kx, ky = kmat(kinit)
     topological(kx, ky, eflag=True, dipflag=True)