7530342ecc25bfad050eccd45289c5691416e850
Patrick Grössing <invarianztheorem@web.de>
2020-08-22 14:52:59 +0200

    Tries at parallel runs.

--- hfsbe/solver/runloops/runloops.py
+++ hfsbe/solver/runloops/runloops.py
@@ -2,9 +2,13 @@ import os
 from hfsbe.solver import sbe_solver
 
 
-def mkdir_chdir(dirname):
+def mkdir(dirname):
     if (not os.path.exists(dirname)):
         os.mkdir(dirname)
+
+
+def mkdir_chdir(dirname):
+    mkdir(dirname)
     os.chdir(dirname)
 
 
@@ -27,3 +31,55 @@ def chirp_phasesweep(chirplist, phaselist, system, dipole, params):
         os.chdir('..')
 
 
+# def parallel_chirp_phasesweep(chirplist, phaselist, system, dipole, params):
+
+#     for chirp in chirplist:
+#         params.chirp = chirp
+#         print("Current chirp: ", params.chirp)
+#         dirname_chirp = 'chirp_{:1.3f}'.format(params.chirp)
+#         mkdir_chdir(dirname_chirp)
+
+#         for phase in phaselist:
+#             params.phase = phase
+#             print("Current phase: ", params.phase)
+#             dirname_phase = 'phase_{:1.2f}'.format(params.phase)
+#             mkdir_chdir(dirname_phase)
+#             p = multiprocessing.Process(target=sbe_solver, args=(system, dipole, params,))
+#             p.start()
+#             p.join()
+#             # sbe_solver(system, dipole, params)
+#             os.chdir('..')
+
+#         os.chdir('..')
+
+# def mpi_chirp_phasesweep(chirplist, phaselist, system, dipole, params):
+
+#     Multi = MpiHelpers()
+#     # Create all chirp folders beforehand
+#     if (Multi.rank == 0):
+#         for chirp in chirplist:
+#             dirname_chirp = 'chirp_{:1.3f}'.format(params.chirp)
+#             mkdir(dirname_chirp)
+
+#     Multi.comm.Barrier()
+
+#     phlist, phlocal, ptuple, displace = Multi.listchop(phaselist)
+#     Multi.comm.Scatterv([phlist, ptuple, displace, MPI.DOUBLE], phlocal)
+
+#     for chirp in chirplist:
+#         params.chirp = chirp
+#         dirname_chirp = 'chirp_{:1.3f}'.format(params.chirp)
+#         print("Rank: ", Multi.rank, " Current chirp: ", params.chirp)
+#         os.chdir(dirname_chirp)
+
+#         for phase in phlocal:
+#             params.phase = phase
+#             print("Rank: ", Multi.rank, " Current phase: ", params.phase)
+#             dirname_phase = 'phase_{:1.2f}'.format(params.phase)
+#             mkdir_chdir(dirname_phase)
+#             sbe_solver(system, dipole, params)
+#             os.chdir('..')
+
+#         os.chdir('..')
+
+#     Multi.comm.Barrier()
