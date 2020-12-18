removed bug if save_full=False; Numerical solver is now accurate for more than two paths up to deviations in the dipole elements; added plot-routine to compare sprecturm from different solving methods

sbe/plotting/fourier_plots.py
@@ -54,7 +54,7 @@ def fourier_dir_ortho(freqw, data_dir, data_ortho, xlim=(0.2, 30),
     ax.set_ylabel(ylabel)
     lines_dir = ax.semilogy(freqw.T, data_dir.T)
     plt.gca().set_prop_cycle(None)
-    lines_ortho = ax.semilogy(freqw.T, data_ortho.T, linestyle='--')
+    lines_ortho = ax.semilogy(freqw.T, data_ortho.T, color='red', linestyle='--')
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
 
@@ -78,6 +78,51 @@ def fourier_dir_ortho(freqw, data_dir, data_ortho, xlim=(0.2, 30),
         plt.show()
     else:
         plt.savefig(savename)
+
+def fourier_ana_num(freqw, data_1, data_2, xlim=(0.2, 30),
+                      ylim=(10e-15, 1),
+                      xlabel=r'Frequency $\omega/\omega_0$',
+                      ylabel=r'$I_\mathrm{hh}$ in atomic units',
+                      paramlegend=None, ls_dir=None, ls_ortho=None,
+                      supertitle=None, title=None, savename=None):
+
+    freqw = freqw.real
+    data_dir = data_1.real
+    data_ortho = data_2.real
+    _fig, ax = plt.subplots(1)
+    ax.set_xticks(np.arange(xlim[1]+1))
+    ax.grid(True, axis='both', ls='--')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    lines_dir = ax.semilogy(freqw.T, data_1.T, label='analytical calculation')
+    plt.gca().set_prop_cycle(None)
+    lines_ortho = ax.semilogy(freqw.T, data_2.T, color='red', linestyle='--', label='numerical calculation')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.legend(loc='upper right')
+
+
+    if ls_dir:
+        for ls, line in zip(ls_dir, lines_dir):
+            line.set_linestyle(ls)
+    if ls_ortho:
+        for ls, line in zip(ls_ortho, lines_ortho):
+            line.set_linestyle(ls)
+
+    if paramlegend is not None:
+        ax.legend(paramlegend)
+
+    if title is not None:
+        plt.title(title)
+
+    if supertitle is not None:
+        plt.suptitle(supertitle)
+
+    if savename is None:
+        plt.show()
+    else:
+        plt.savefig(savename)
+
 
 
 def fourier_dir_ortho_split(freqw, data_dir, data_ortho, xlim=(0.2, 30),

sbe/solver/observables_n_bands.py
@@ -127,9 +127,6 @@ def make_matrix_elements_hderiv(params, hamiltonian, paths, wf, E_dir, path_idx)
             hgridminusy[i, j, :, :] = hamiltonian(kx=kx, ky=ky-epsilon)
     dhdkx = ( hgridplusx -  hgridminusx )/(2*epsilon)
     dhdky = ( hgridplusy -  hgridminusy )/(2*epsilon)
-    
-    print(dhdkx[0, 0, 0, 1])
-    print(dhdky[0, 0, 0, 1])
 
     matrix_element_x = np.empty([Nk_in_path, num_paths, n, n], dtype=np.complex128)
     matrix_element_y = np.empty([Nk_in_path, num_paths, n, n], dtype=np.complex128)
@@ -210,7 +207,7 @@ def make_matrix_elements_dipoles(params, hamiltonian, paths, dipole_in_path, dip
 
     return mel_in_path.reshape(Nk_in_path, n, n), mel_ortho.reshape(Nk_in_path, n, n)
 
-def current_per_path(params, Nt, mel_in_path, mel_ortho, density_matrix, path_idx):
+def current_per_path(params, Nt, mel_in_path, mel_ortho, density_matrix):
 
     n = params.n
 
@@ -222,13 +219,13 @@ def current_per_path(params, Nt, mel_in_path, mel_ortho, density_matrix, path_id
 
     for i_t in range(Nt):
         for i in range(n):
-            current_in_path[i_t] += - np.sum(mel_in_path[:, i, i] * density_matrix[:, path_idx, i_t, i, i].real)
-            current_in_path_intraband[i_t] += - np.sum(mel_in_path[:, i, i] * density_matrix[:, path_idx, i_t, i, i].real)
-            current_ortho[i_t] += - np.sum(mel_ortho[:, i, i] * density_matrix[:, path_idx, i_t, i, i].real)
-            current_ortho_intraband[i_t] += - np.sum(mel_ortho[:, i, i] * density_matrix[:, path_idx, i_t, i, i].real)
+            current_in_path[i_t] += - np.sum(mel_in_path[:, i, i] * density_matrix[:, i_t, i, i].real)
+            current_in_path_intraband[i_t] += - np.sum(mel_in_path[:, i, i] * density_matrix[:, i_t, i, i].real)
+            current_ortho[i_t] += - np.sum(mel_ortho[:, i, i] * density_matrix[:, i_t, i, i].real)
+            current_ortho_intraband[i_t] += - np.sum(mel_ortho[:, i, i] * density_matrix[:, i_t, i, i].real)
             for j in range(n):        
                 if i != j:
-                    current_in_path[i_t] += - np.sum(np.real(mel_in_path[:, i, j] * density_matrix[:, path_idx, i_t, j, i]))
-                    current_ortho[i_t] += - np.sum(np.real(mel_ortho[:, i, j] * density_matrix[:, path_idx, i_t, j, i]))
+                    current_in_path[i_t] += - np.sum(np.real(mel_in_path[:, i, j] * density_matrix[:,  i_t, j, i]))
+                    current_ortho[i_t] += - np.sum(np.real(mel_ortho[:, i, j] * density_matrix[:,  i_t, j, i]))
 
     return current_in_path, current_in_path_intraband, current_ortho, current_ortho_intraband

sbe/solver/solver_n_bands.py
@@ -14,7 +14,7 @@ from sbe.solver import make_electric_field
 from sbe.solver import make_matrix_elements_hderiv, make_matrix_elements_dipoles, current_per_path
 from sbe.dipole import diagonalize, dipole_elements
 
-def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
+def sbe_solver_n_bands(params, sys, dipole, curvature):
     """
         Solver for the semiconductor bloch equation ( eq. (39) or (47) in https://arxiv.org/abs/2008.03177)
         for a n band system with numerical calculation of the dipole elements (unfinished - analytical dipoles
@@ -199,6 +199,11 @@ def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
             print("Calculating dipoles...")
               
         dipole_x, dipole_y = dipole_elements(params, hnp, paths)
+
+    # Only define full density matrix solution if save_full is True
+    if save_full:
+        solution_full = np.empty((Nk1, Nk2, Nt, 4), dtype=np.complex128)
+
     ###########################################################################
     # SOLVING
     ###########################################################################
@@ -206,9 +211,6 @@ def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
 
     for Nk2_idx, path in enumerate(paths):
         print("Solving SBE for Path: ", Nk2_idx + 1)
-        if not save_full:
-            # If we don't need the full solution only operate on path idx 0
-            Nk2_idx = 0
 
         # Retrieve the set of k-points for the current path
         kx_in_path = path[:, 0]
@@ -275,7 +277,12 @@ def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
             # Do not append the last element (A_field)
             # If save_full is False Nk2_idx is 0 as only the current path
             # is saved
-            solution[:, Nk2_idx, ti, :, :] = solver.y[:-1].reshape(Nk1, n, n)
+            solution[:, ti, :, :] = solver.y[:-1].reshape(Nk1, n, n)
+
+            # Only write full density matrix solution if save_full is True
+            if save_full:
+                solution_full[:, Nk2_idx, ti, :] = solution
+
             # Construct time array only once
             if not t_constructed:
                 # Construct time and A_field only in first round
@@ -292,10 +299,10 @@ def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
         if user_out: 
             print("Calculating emission of the current path...")
 
-        #mel_in_path, mel_ortho = make_matrix_elements_dipoles(params, hnp, paths, dipole_in_path, dipole_ortho, e_in_path, E_dir, Nk2_idx)
-        mel_in_path, mel_ortho = make_matrix_elements_hderiv(params, hnp, paths, wf, E_dir, Nk2_idx)      
+        mel_in_path, mel_ortho = make_matrix_elements_dipoles(params, hnp, paths, dipole_in_path, dipole_ortho, e_in_path, E_dir, Nk2_idx)
+        #mel_in_path, mel_ortho = make_matrix_elements_hderiv(params, hnp, paths, wf, E_dir, Nk2_idx)     # NOT ACCURATE (YET!) 
 
-        current_in_path, current_in_path_intraband, current_ortho, current_ortho_intraband = current_per_path(params, Nt, mel_in_path, mel_ortho, solution, Nk2_idx)
+        current_in_path, current_in_path_intraband, current_ortho, current_ortho_intraband = current_per_path(params, Nt, mel_in_path, mel_ortho, solution)
         
         emission_exact_E_dir += current_in_path
         emission_intraband_E_dir += current_in_path_intraband
@@ -331,7 +338,7 @@ def sbe_solver_n_bands(params, sys, dipole, curvature, wf_sym):
 
     if save_full:
         S_name = 'Sol_' + tail
-        np.savez(S_name, t=t, solution=solution, paths=paths,
+        np.savez(S_name, t=t, solution_full=solution_full, paths=paths,
                  electric_field=electric_field(t), A_field=A_field)
 
 def make_fnumba(n, E_dir, gamma1, gamma2, electric_field, params, dk, gauge):
@@ -424,12 +431,9 @@ def solution_containers(Nk1, Nk2, Nt, n, save_approx, save_full, zeeman=False):
 
     # The solution array is structred as: first index is Nk1-index,
     # second is Nk2-index, third is timestep, fourth is f_h, p_he, p_eh, f_e
-    if save_full:
-        # Make container for full solution if it is needed
-        solution = np.empty((Nk1, Nk2, Nt, n, n), dtype=np.complex128)
-    else:
-        # Only one path needed at a time if no full solution is needed
-        solution = np.empty((Nk1, 1, Nt, n, n), dtype=np.complex128)
+
+    # Only one path needed at a time if no full solution is needed
+    solution = np.empty((Nk1, Nt, n, n), dtype=np.complex128)
 
     A_field = np.empty(Nt, dtype=np.float64)
     E_field = np.empty(Nt, dtype=np.float64)