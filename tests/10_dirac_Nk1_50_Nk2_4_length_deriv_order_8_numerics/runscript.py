a3decba69eb78e9e7cfc2c19ec61e850a33ffd4f
Adrian <adrian.seith@ur.de>
2021-01-21 18:38:27 +0100

    added test for n-band solver

---
+++ tests/10_dirac_Nk1_50_Nk2_4_length_deriv_order_8_numerics/runscript.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import importlib
+from params import params
+
+import sbe.dipole
+import sbe.example
+from sbe.solver import sbe_solver_n_bands
+
+def dirac():
+    A = 0.1974      # Fermi velocity
+
+    dirac_system = sbe.example.BiTe(C0=0, C2=0, A=A, R=0, mz=0)
+    h_sym, ef_sym, wf_sym, _ediff_sym = dirac_system.eigensystem(gidx=1)
+    dirac_dipole = sbe.dipole.SymbolicDipole(h_sym, ef_sym, wf_sym)
+    dirac_curvature = sbe.dipole.SymbolicCurvature(h_sym, dirac_dipole.Ax, dirac_dipole.Ay)
+
+    return dirac_system, dirac_dipole, dirac_curvature
+
+def run(system, dipole, curvat):
+
+    sbe_solver_n_bands(system, dipole, params, curvat)
+
+    return 0
+
+if __name__ == "__main__":
+    run(*dirac())
