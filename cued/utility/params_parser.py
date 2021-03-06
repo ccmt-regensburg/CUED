1fb1445ebea771dcdbb73b149638244d3cbf3651
Patrick Grössing <invarianztheorem@web.de>
2021-03-06 17:14:58 +0100

    Logical splitting of code parameters for easier understanding.

--- cued/utility/params_parser.py
+++ cued/utility/params_parser.py
@@ -10,135 +10,132 @@ class Params():
     Additional check for ill-defined user parameters included.
     """
     def __init__(self, UP):
-        # Command line progress output
-        self.user_out = True
-        if hasattr(UP, 'user_out'):
-            self.user_out = UP.user_out
 
-        # Save full density matrix
-        self.save_full = False
-        if hasattr(UP, 'save_full'):
-            self.save_full = UP.save_full
+        self.__occupation(UP)
+        self.__time_scales(UP)
+        self.__field(UP)
+        self.__brillouin_zone(UP)
+        self.__optional(UP)
 
-        # Save data as human readable PDF file
-        self.save_latex_pdf = False
-        if hasattr(UP, 'save_latex_pdf'):
-            self.save_latex_pdf = UP.save_latex_pdf
+        # Check if user params has any ill-defined parameters
+        self.__check_user_paramas_for_wrong_arguments(UP)
+        self.__append_derived_parameters(UP)
 
-        # Save j^intra, j^anom, dP^inter/dt
-        self.split_current = False
-        if hasattr(UP, 'split_current'):
-            self.split_current = UP.split_current
-
-        # Semiclassical calc. (dipole = 0)
-        self.do_semicl = False
-        if hasattr(UP, 'do_semicl'):
-            self.do_semicl = UP.do_semicl
-
-        # Gauge of the SBE Dynamics
-        self.gauge = 'length'
-        if hasattr(UP, 'gauge'):
-            self.gauge = UP.gauge
-
-        # Numerical or analytical calculation of eigenstates and dipoles
-        self.hamiltonian_evaluation = 'num'
-        if hasattr(UP, 'hamiltonian_evaluation'):
-            self.hamiltonian_evaluation = UP.hamiltonian_evaluation
-
-        # 2 or n band solver
-        self.solver = 'nband'
-        if hasattr(UP, 'solver'):
-            self.solver = UP.solver
-
-        # Step size for num. derivatives
-        self.epsilon = 2e-5
-        if hasattr(UP, 'epsilon'):
-            self.epsilon = UP.epsilon
-
-        # Index of real wave function entry (fixes gauge)
-        self.gidx = 1
-        if hasattr(UP, 'gidx'):
-            self.gidx = UP.gidx
-
-        # Calculate anomalous current
-        self.save_anom = False
-        if hasattr(UP, 'save_anom'):
-            self.save_anom = UP.save_anom
-
-        # 'adams' non-stiff, 'bdf' stiff, 'rk4' Runge-Kutta 4th order
-        self.solver_method = 'bdf'
-        if hasattr(UP, 'solver_method'):
-            self.solver_method = UP.solver_method
-
-        # Quadruple for reducing numerical noise
-        self.precision = 'double'
-        if hasattr(UP, 'precision'):
-            self.precision = UP.precision
-
-        # Special flag for accurate insulator calc.
-        self.symmetric_insulator = False
-        if hasattr(UP, 'symmetric_insulator'):
-            self.symmetric_insulator = UP.symmetric_insulator
-
-        # Accuracy order of density-matrix k-deriv.
-        self.dk_order = 8
-        if hasattr(UP, 'dk_order'):
-            self.dk_order = UP.dk_order                 # with length gauge (avail: 2,4,6,8)
-            if self.dk_order not in [2, 4, 6, 8]:
-                sys.exit("dk_order needs to be either 2, 4, 6, or 8.")
-
-        # Parameters for initial occupation
+    def __occupation(self, UP):
+        '''Parameters for initial occupation'''
         self.e_fermi = UP.e_fermi*CoFa.eV_to_au           # Fermi energy
         self.temperature = UP.temperature*CoFa.eV_to_au   # Temperature
 
-        # Driving field parameters
+    def __time_scales(self, UP):
+        '''Time Scales'''
+        self.T1 = UP.T1*CoFa.fs_to_au                     # Occupation damping time
+        self.T2 = UP.T2*CoFa.fs_to_au                     # Polarization damping time
+        self.t0 = UP.t0*CoFa.fs_to_au
+        self.dt = UP.dt*CoFa.fs_to_au
+
+    def __field(self, UP):
+        '''Electrical Driving Field'''
         self.f = UP.f*CoFa.THz_to_au                      # Driving pulse frequency
         self.E0 = UP.E0*CoFa.MVpcm_to_au                  # Driving pulse field amplitude
         self.chirp = UP.chirp*CoFa.THz_to_au              # Pulse chirp frequency
         self.sigma = UP.sigma*CoFa.fs_to_au               # Gaussian pulse width
         self.phase = UP.phase                             # Carrier-envelope phase
 
-        # Time scales
-        self.T1 = UP.T1*CoFa.fs_to_au                     # Occupation damping time
-        self.T2 = UP.T2*CoFa.fs_to_au                     # Polarization damping time
-        self.t0 = UP.t0*CoFa.fs_to_au
-        self.dt = UP.dt*CoFa.fs_to_au
+    def __brillouin_zone(self, UP):
+        '''Brillouin zone/Lattice''' 
+        self.BZ_type = UP.BZ_type                         # Type of Brillouin zone
+        self.Nk1 = UP.Nk1                                 # kpoints in b1 direction
+        self.Nk2 = UP.Nk2                                 # kpoints in b2 direction
+
+        if self.BZ_type == 'hexagon':
+            self.align = UP.align                         # E-field alignment
+            self.angle_inc_E_field = None
+            self.a = UP.a                                 # Lattice spacing
+        elif self.BZ_type == 'rectangle':
+            self.align = None
+            self.angle_inc_E_field = UP.angle_inc_E_field
+            self.length_BZ_ortho = UP.length_BZ_ortho     # Size of the Brillouin zone in atomic units
+            self.length_BZ_E_dir = UP.length_BZ_E_dir     # -"-
+        else:
+            sys.exit("BZ_type needs to be either hexagon or rectangle.")
+
+    def __optional(self, UP):
+        '''Optional parameters or default values'''
+        self.user_out = True                              # Command line progress output
+        if hasattr(UP, 'user_out'):
+            self.user_out = UP.user_out
+
+        self.save_full = False                            # Save full density matrix
+        if hasattr(UP, 'save_full'):
+            self.save_full = UP.save_full
+
+        self.save_latex_pdf = False                       # Save data as human readable PDF file
+        if hasattr(UP, 'save_latex_pdf'):
+            self.save_latex_pdf = UP.save_latex_pdf
+
+        self.split_current = False                        # Save j^intra, j^anom, dP^inter/dt
+        if hasattr(UP, 'split_current'):
+            self.split_current = UP.split_current
+
+        self.do_semicl = False                            # Semiclassical calc. (dipole = 0)
+        if hasattr(UP, 'do_semicl'):
+            self.do_semicl = UP.do_semicl
+
+        self.gauge = 'length'                             # Gauge of the SBE Dynamics
+        if hasattr(UP, 'gauge'):
+            self.gauge = UP.gauge
+
+        self.hamiltonian_evaluation = 'num'               # Numerical or analytical calculation of eigenstates and dipoles
+        if hasattr(UP, 'hamiltonian_evaluation'):
+            self.hamiltonian_evaluation = UP.hamiltonian_evaluation
+
+        self.solver = 'nband'                             # 2 or n band solver
+        if hasattr(UP, 'solver'):
+            self.solver = UP.solver
+
+        self.epsilon = 2e-5                               # Step size for num. derivatives
+        if hasattr(UP, 'epsilon'):
+            self.epsilon = UP.epsilon
+
+        self.gidx = 1                                     # Index of real wave function entry (fixes gauge)
+        if hasattr(UP, 'gidx'):
+            self.gidx = UP.gidx
+
+        self.save_anom = False                            # Calculate anomalous current
+        if hasattr(UP, 'save_anom'):
+            self.save_anom = UP.save_anom
+
+        self.solver_method = 'bdf'                        # 'adams' non-stiff, 'bdf' stiff, 'rk4' Runge-Kutta 4th order
+        if hasattr(UP, 'solver_method'):
+            self.solver_method = UP.solver_method
+
+        self.precision = 'double'                         # Quadruple for reducing numerical noise
+        if hasattr(UP, 'precision'):
+            self.precision = UP.precision
+
+        self.symmetric_insulator = False                  # Special flag for accurate insulator calc.
+        if hasattr(UP, 'symmetric_insulator'):
+            self.symmetric_insulator = UP.symmetric_insulator
+
+        
+        self.dk_order = 8                                 # Accuracy order of density-matrix k-deriv.
+        if hasattr(UP, 'dk_order'):
+            self.dk_order = UP.dk_order                   # with length gauge (avail: 2,4,6,8)
+            if self.dk_order not in [2, 4, 6, 8]:
+                sys.exit("dk_order needs to be either 2, 4, 6, or 8.")
 
         self.factor_freq_resolution = 1
         if hasattr(UP, 'factor_freq_resolution'):
             self.factor_freq_resolution = UP.factor_freq_resolution
 
-        # gaussian or hann
-        self.fourier_window_function = 'hann'
+        
+        self.fourier_window_function = 'hann'             # gaussian or hann
         if hasattr(UP, 'fourier_window_function'):
             self.fourier_window_function = UP.fourier_window_function
 
         self.gaussian_window_width = self.sigma
         if hasattr(UP, 'gaussian_window_width'):
             self.gaussian_window_width = UP.gaussian_window_width*CoFa.fs_to_au
-
-        # Brillouin zone type
-        self.BZ_type = UP.BZ_type                      # Type of Brillouin zone
-        self.Nk1 = UP.Nk1                              # kpoints in b1 direction
-        self.Nk2 = UP.Nk2                              # kpoints in b2 direction
-
-        # Parameters for individual Brillouin zone types
-        if self.BZ_type == 'hexagon':
-            self.align = UP.align                      # E-field alignment
-            self.angle_inc_E_field = None
-            self.a = UP.a                              # Lattice spacing
-        elif self.BZ_type == 'rectangle':
-            self.align = None
-            self.angle_inc_E_field = UP.angle_inc_E_field
-            self.length_BZ_ortho = UP.length_BZ_ortho  # Size of the Brillouin zone in atomic units
-            self.length_BZ_E_dir = UP.length_BZ_E_dir  # -"-
-        else:
-            sys.exit("BZ_type needs to be either hexagon or rectangle.")
-
-        # Check if user params has any ill-defined parameters
-        self.__check_user_paramas_for_wrong_arguments(UP)
-
-        self.__append_derived_parameters(UP)
 
     def __check_user_paramas_for_wrong_arguments(self, UP):
         """
