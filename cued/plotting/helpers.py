185427b13fa62167f0cfd6b1a6ffb01206e7aa5a
Invarianz <invarianztheorem@web.de>
2022-01-09 18:43:51 +0100

    Small rename for better reference in scripts.

--- cued/plotting/helpers.py
+++ cued/plotting/helpers.py
@@ -1,8 +1,8 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-labels = ['(' + chr(ord('a') + i) + ')' for i in range(26)]
-colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+default_labels = ['(' + chr(ord('a') + i) + ')' for i in range(26)]
+default_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 def label_inner(ax, idx=None, color='black'):
     '''
@@ -11,10 +11,10 @@ def label_inner(ax, idx=None, color='black'):
     If idx is given it puts that label
     '''
     if idx is None:
-        ax.text(0.02, 0.88, r'{}'.format(labels[plt_fig_idx_inner.counter]), transform=ax.transAxes, color=color)
+        ax.text(0.02, 0.88, r'{}'.format(default_labels[plt_fig_idx_inner.counter]), transform=ax.transAxes, color=color)
         label_inner.counter += 1
     else:
-        ax.text(0.02, 0.88, r'{}'.format(labels[idx]), transform=ax.transAxes, color=color)
+        ax.text(0.02, 0.88, r'{}'.format(default_labels[idx]), transform=ax.transAxes, color=color)
 
 label_inner.counter = 0
 
