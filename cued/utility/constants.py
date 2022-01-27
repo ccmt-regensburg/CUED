b23d361d0ac6405c95a6e1b9e41fe8d4e17acd2b
Invarianz <invarianztheorem@web.de>
2022-01-27 15:25:40 +0100

    Reversal of using scipy.constants -> Problem with test files.

--- cued/utility/constants.py
+++ cued/utility/constants.py
@@ -5,32 +5,52 @@ class ConversionFactors:
 	'''
 	Collection of conversion factors from SI to atomic units
 	'''
+	##################################################
+	# SI <-> a.u. conversions
+	##################################################
+	# Better accuracy -> Problems with test files
 	# (1fs = 41.3414733358211 a.u.)
-	fs_to_au = sco.femto/psco['atomic unit of time'][0]
-	au_to_fs = psco['atomic unit of time'][0]/sco.femto
+	# fs_to_au = sco.femto/psco['atomic unit of time'][0]
+	# au_to_fs = psco['atomic unit of time'][0]/sco.femto
 
-	MVpcm_to_au = 0.0001944690381        # (1MV/cm = 1.944690381*10^-4 a.u.)
+	# (1fs = 41.341473335 a.u.)
+	fs_to_au = 41.34137335
+	au_to_fs = 1/fs_to_au
+
+	# (1MV/cm = 1.944690381*10^-4 a.u.)
+	MVpcm_to_au = 0.0001944690381
 	au_to_MVpcm = 1/MVpcm_to_au
 
-	THz_to_au = 0.000024188843266        # (1THz   = 2.4188843266*10^-5 a.u.)
+	# (1THz   = 2.4188843266*10^-5 a.u.)
+	THz_to_au = 0.000024188843266
 	au_to_THz = 1/THz_to_au
 
-	Amp_to_au = 150.97488474             # (1A     = 150.97488474)
+	# (1A     = 150.97488474)
+	Amp_to_au = 150.97488474
 	au_to_Amp = 1/Amp_to_au
 
-	eV_to_au = 0.03674932176             # (1eV    = 0.036749322176 a.u.)
+	# (1eV    = 0.036749322176 a.u.)
+	eV_to_au = 0.03674932176
 	au_to_eV = 1/eV_to_au
 
-	as_to_au = 1.8897261254535           # (1Angst.= 1.8897261254535 a.u.)
+	# (1Angst.= 1.8897261254535 a.u.)
+	as_to_au = 1.8897261254535
 	au_to_as = 1/as_to_au
 
+	##################################################
 	# Magnetic field constants
-	T_to_au = 4.25531e-6                 # (1T = 4.25531e-6 a.u.)
+	##################################################
+	# (1T = 4.25531e-6 a.u.)
+	T_to_au = 4.25531e-6
 	au_to_T = 1/T_to_au
 
-	muB_to_au = 0.5                      # (1mu_b = 0.5 a.u.)
+	# (1mu_b = 0.5 a.u.)
+	muB_to_au = 0.5
 	au_to_muB = 1/muB_to_au
 
+	##################################################
 	# SI conversions
-	eV_to_THz = 241.7991                 # (1THz = 241.7991 eV)
+	##################################################
+	# (1THz = 241.7991 eV)
+	eV_to_THz = 241.7991
 	THz_to_eV = 1/eV_to_THz
