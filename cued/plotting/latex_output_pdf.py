fc8bb6a9941e218ffead981b872f4f04f1568219
Jan Wilhelm <JWilhelm@users.noreply.github.com>
2022-02-08 14:06:03 +0100

    High-symmetry line for printing band struc + dipoles: go until the end point

--- cued/plotting/latex_output_pdf.py
+++ cued/plotting/latex_output_pdf.py
@@ -242,10 +242,10 @@ def get_symmetry_path_in_BZ(P, num_points_for_plotting):
 	Nk_per_line = num_points_for_plotting//2
 
 	delta = 1/(2*Nk_per_line)
-	neg_array_direct  = np.linspace(-0.5+delta,	 0.0-delta, num=Nk_per_line)
-	neg_array_reverse = np.linspace( 0.0+delta, -0.5-delta, num=Nk_per_line)
-	pos_array_direct  = np.linspace( 0.0+delta,	 0.5-delta, num=Nk_per_line)
-	pos_array_reverse = np.linspace( 0.5-delta,	 0.0+delta, num=Nk_per_line)
+	neg_array_direct  = np.linspace(-1.0+delta, 0.0-delta, num=Nk_per_line)
+	neg_array_reverse = np.linspace( 0.0+delta, -1.0-delta, num=Nk_per_line)
+	pos_array_direct  = np.linspace( 0.0+delta, 1.0-delta, num=Nk_per_line)
+	pos_array_reverse = np.linspace( 1.0-delta, 0.0+delta, num=Nk_per_line)
 
 	if P.BZ_type == 'hexagon':
 
